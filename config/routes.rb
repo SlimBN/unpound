0f190f801e374448f123d0f56b90e644f416c864
slimBN <elliji@yopmail.com>
2013-10-25 06:43:58 +0100

    tarki7 version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Unbound::Application.routes.draw do
 
+  root :to => 'soons#index'
+
 
   resources :visits
 
@@ -129,8 +131,6 @@ Unbound::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'homes#index'
-
-  root :to => 'soons#index'
 
   # See how all your routes lay out with "rake routes"
 
